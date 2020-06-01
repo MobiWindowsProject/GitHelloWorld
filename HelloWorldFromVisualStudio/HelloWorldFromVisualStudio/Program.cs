c9dcfc319214ca08e0914c4203cb72cb8e1b8c38
Mubashir Hussain <mubashir.hussain@live.com>
2020-06-01 21:13:29 +1000

    My first code from Vs to Git

---
+++ HelloWorldFromVisualStudio/HelloWorldFromVisualStudio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldFromVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello I am a Visual Studio Code.");
+            Console.ReadLine();
+        }
+    }
+}
